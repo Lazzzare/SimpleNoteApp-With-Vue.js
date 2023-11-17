f66e496c34c251a0e045ab73435e1d0ae6c40e78
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-17 18:54:25 +0400

    Finish

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from "vue";
+import { Icon } from "@iconify/vue";
 
 const showOverlay = ref(false);
 const input = ref("");
 const todos = ref([]);
 const errorMessage = ref("");
+const darkMode = ref(false);
 
 const getRandomColor = () => {
   const hue = Math.floor(Math.random() * 360);
@@ -28,10 +30,14 @@ const addTodo = () => {
   input.value = "";
   errorMessage.value = "";
 };
+
+const deleteTodo = (id) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
 </script>
 
 <template>
-  <div class="w-full min-h-screen bg-slate-200">
+  <div :class="{ dark: darkMode }" class="w-full min-h-screen bg-slate-200">
     <!-- Overlay -->
     <div
       v-if="showOverlay"
@@ -42,6 +48,7 @@ const addTodo = () => {
       >
         <textarea
           v-model="input"
+          :class="{ dark: darkMode }"
           class="border border-gray-400 focus:outline-none p-4"
           placeholder="Write Todo Here..."
           name="todo"
@@ -66,6 +73,22 @@ const addTodo = () => {
       class="max-w-[1000px] flex flex-row items-center justify-between mx-auto pt-20 text-4xl"
     >
       <h1>Notes</h1>
+      <div>
+        <Icon
+          @click="darkMode = false"
+          v-show="darkMode"
+          icon="openmoji:sun-with-face"
+          width="30"
+          class="cursor-pointer mt-3"
+        />
+        <Icon
+          @click="darkMode = true"
+          v-show="!darkMode"
+          icon="openmoji:last-quarter-moon-face"
+          width="30"
+          class="cursor-pointer mt-3"
+        />
+      </div>
       <button
         @click="showOverlay = true"
         class="bg-black text-white items-center text-center rounded-full w-12 h-12"
@@ -84,8 +107,30 @@ const addTodo = () => {
         <h1 class="whitespace-normal break-words">
           {{ todo.text }}
         </h1>
-        <p class="font-bold">{{ todo.date.toLocaleDateString("en-US") }}</p>
+        <div class="flex flex-row items-center justify-between">
+          <p class="font-bold">{{ todo.date.toLocaleDateString("en-US") }}</p>
+          <Icon
+            @click="deleteTodo(todo.id)"
+            icon="material-symbols-light:delete"
+            width="25"
+            class="cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+body {
+  background-color: #f3f4f6;
+}
+
+.dark {
+  background-color: #1f2937;
+}
+
+.dark {
+  color: white;
+}
+</style>

5b3ba047abeddb1667f1a9c4c4c45d197378fb84
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-17 17:56:16 +0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,72 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const showOverlay = ref(false);
+const input = ref("");
+const todos = ref([]);
+
+const addTodo = () => {
+  todos.value.push({
+    id: Math.floor(Math.random() * 100000),
+    text: input.value,
+    date: new Date(),
+  });
+  showOverlay.value = false;
+  input.value = "";
+};
+</script>
 
 <template>
-  <h1 class="text-red-500 font-bold">Hello</h1>
+  <div class="w-full min-h-screen bg-slate-200">
+    <!-- Overlay -->
+    <div
+      v-if="showOverlay"
+      class="absolute w-full h-screen bg-[rgb(0,0,0,0.77)] flex justify-center items-center"
+    >
+      <div
+        class="max-w-[1000px] w-[750px] bg-white rounded-[10px] p-8 relative flex flex-col"
+      >
+        <textarea
+          v-model="input"
+          class="border border-gray-400 focus:outline-none p-4"
+          placeholder="Write Todo Here..."
+          name="todo"
+          id="todo"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <div class="flex flex-col gap-4 mt-10">
+          <button @click="addTodo" class="bg-blue-400 py-2">Add Todo</button>
+          <button @click="showOverlay = false" class="bg-red-500 py-2">
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- Header -->
+    <header
+      class="max-w-[1000px] flex flex-row items-center justify-between mx-auto pt-20 text-4xl"
+    >
+      <h1>Notes</h1>
+      <button
+        @click="showOverlay = true"
+        class="bg-black text-white items-center text-center rounded-full w-12 h-12"
+      >
+        +
+      </button>
+    </header>
+    <!-- Notes Card -->
+    <div class="flex flex-row flex-wrap max-w-[1000px] mt-28 mx-auto gap-12">
+      <div
+        v-for="todo in todos"
+        :key="todo.id"
+        class="bg-yellow-300 w-[250px] h-[300px] rounded-lg flex flex-col justify-between p-4"
+      >
+        <h1>
+          {{ todo.text }}
+        </h1>
+        <p class="font-bold">{{ todo.date.toLocaleDateString("en-US") }}</p>
+      </div>
+    </div>
+  </div>
 </template>
